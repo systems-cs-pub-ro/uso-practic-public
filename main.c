f7953f24f2f61001132b21d7b38331383471a908
Andrei Martin <andrei.cos.martin@gmail.com>
2021-12-03 22:56:45 +0200

    Add resources for var 75

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("You don’t trust people because they are trustworthy.\n");
+	printf("You do it because you have nothing else to rely on.\n");
+
+	return 0;
+}
