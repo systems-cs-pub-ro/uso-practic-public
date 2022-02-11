964b284a0b92c11807963e9b7580b1d2c3520525
Andrei Martin <andrei.cos.martin@gmail.com>
2022-02-11 14:11:37 +0200

    Added quick sort and called it in main to sort the array.

--- main.c
+++ main.c
@@ -2,6 +2,39 @@
 
 #define SIZE 7
 
+void swap(int *a, int *b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int array[], int low, int high) {
+  
+	int pivot = array[high];
+	int i = (low - 1);
+
+	for (int j = low; j < high; j++) {
+		if (array[j] <= pivot) {
+		  i++;
+		  swap(&array[i], &array[j]);
+		}
+	}
+
+	swap(&array[i + 1], &array[high]);
+  
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+
+	if (low < high) {
+		int pi = partition(array, low, high);
+
+		quickSort(array, low, pi - 1);
+		quickSort(array, pi + 1, high);
+	}
+}
+
 // function to print array elements
 void printArray(int array[], int size) {
 	for (int i = 0; i < size; ++i) {
@@ -23,6 +56,11 @@ int main(int argc, char *argv[]) {
 	n = sizeof(data) / sizeof(data[0]);
 
 	printf("Unsorted Array: \n");
+	printArray(data, n);
+
+	quickSort(data, 0, n - 1);
+
+	printf("Sorted array in ascending order: \n");
 	printArray(data, n);
 
 }
