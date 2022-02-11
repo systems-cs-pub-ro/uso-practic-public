c8a4c789be18ebc1fafb27089020d6b89d5fcbf5
Andrei Martin <andrei.cos.martin@gmail.com>
2022-02-11 14:12:54 +0200

    Modified binary search function.

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ int binarySearch(int arr[], int l, int r, int x)
             return mid;
  
         if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x)
+            return binarySearch(arr, l, mid - 1, x);
  
         return binarySearch(arr, mid + 1, r, x);
     }
